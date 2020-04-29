2e3ba4566be44f7776dfd3d2b27e92171adff18d
Bala <balalistens@gmail.com>
2020-04-29 15:55:10 +0800

    Versioned API route
    - for backward compatiblity

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DEBUG = os.getenv("FLASK_ENV") == "development"
-APPLICATION_ROOT = os.getenv("APPLICATION_ROOT", "/api")
+APPLICATION_ROOT = os.getenv("APPLICATION_ROOT", "/v1/api")
 HOST = os.getenv("APPLICATION_HOST", "127.0.0.1")
 PORT = int(os.getenv("APPLICATION_PORT", "3000"))
 JSON_API_URL = "https://jsonplaceholder.typicode.com"
